06c8a49a221d276aabbd9558d20a0b62d92a653d
Jogesh <jogesh6895@gmail.com>
2020-03-15 15:12:56 +0530

    Initial Commit

---
+++ LiveSentimentAnalysis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiveSentimentAnalysis
+//
+//  Created by Jogesh Ghadai on 15/03/20.
+//  Copyright © 2020 Jogesh Ghadai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LiveSentimentAnalysis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LiveSentimentAnalysis
+//
+//  Created by Jogesh Ghadai on 15/03/20.
+//  Copyright © 2020 Jogesh Ghadai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
